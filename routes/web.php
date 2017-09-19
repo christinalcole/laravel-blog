2a4c6fb74d9f1af81fa5857f401406aba63a2ac0
Christina L. Cole <christinalcole@gmail.com>
2017-09-19 09:00:04 -0400

    add tasks table, route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = DB::table('tasks')->get();
+    return $tasks;
+    // return view('welcome');
 });
 
 Route::get('/about', function() {
